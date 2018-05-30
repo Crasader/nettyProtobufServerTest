f46e99337754c0f22999476b47a1a155d9d52e08
田宏 <tianhong@edaijia-inc.cn>
2018-05-30 13:50:23 +0800

    del db

--- src/main/resources/script/mysql/demo.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `t_demo` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `user_name` varchar(10) NOT NULL COMMENT '用户名',
-  `title` varchar(3000) DEFAULT '',
-  `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间,yyyymmddhhmmss',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
